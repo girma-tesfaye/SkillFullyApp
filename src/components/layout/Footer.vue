2d5bf198b0673e6b784ed9541975f3b8407bd72e
esuba <esuba419@gmail.com>
2025-04-30 12:11:19 -0400

    fix: add Footer component

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+<template>
+    <v-footer height="64" class="d-flex align-center justify-center ga-2 flex-wrap py-3" color="surface-light" app>
+      <div class="flex-1-0-100 text-center mt-2">
+        {{ new Date().getFullYear() }} — SkillFully
+      </div>
+    </v-footer>
+</template>
